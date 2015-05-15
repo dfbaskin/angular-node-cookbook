079ea98fba6142474893bc396027ff297f14ea03
Dave Baskin <dbaskin@wintellect.com>
2015-05-15 16:39:00 -0400

    Adding express server.

---
+++ server.js
@@ -0,0 +1,19 @@
+
+var portNum = process.env.PORT || 9999;
+
+var express = require('express');
+var app = express();
+
+app.use("/", express.static("src"));
+
+var server = app.listen(portNum, function () {
+    var addr = server.address().address;
+    var host = isLocalHost(addr) ? "localhost" : addr;
+    var port = server.address().port;
+    console.log('Web application listening at http://%s:%s', host, port);
+});
+
+function isLocalHost(addr) {
+    return addr === "::";
+}
+
